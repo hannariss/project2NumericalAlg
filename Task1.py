6b47ca9c95f051f8720ff25eed2c27d2f1f53368
hannariss <rissbacher.h@gmail.com>
2024-09-19 15:38:52 +0200

    chore(task 2): add GeneralOptiMethods

--- Task1.py
+++ Task1.py
@@ -4,3 +4,20 @@ class OptimizationProblem:
         self.objective_func = objective_func
         self.gradient = gradient
 
+class GeneralOptimizationMethod:
+    def __init__(self, func, grad, x0, tol=1e-5, max_iter=1000):
+        """
+        Initialize the optimization method.
+
+        Parameters:
+        - func: The objective function to minimize.
+        - grad: The gradient of the objective function.
+        - x0: Initial guess for the minimum.
+        - tol: Tolerance for the stopping criterion.
+        - max_iter: Maximum number of iterations.
+        """
+        self.func = func
+        self.grad = grad
+        self.x0 = x0
+        self.tol = tol
+        self.max_iter = max_iter
