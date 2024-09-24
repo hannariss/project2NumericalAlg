8f0563cfff073bd93b98c6617c5137a204932659
grimmjule <grimmjule262@gmail.com>
2024-09-24 15:59:59 +0200

    fix(Task1): change from norm (of cross product) to scalar product (np.dot)

--- Task1.py
+++ Task1.py
@@ -86,9 +86,9 @@ class ClassicalNewtonMethod(GeneralOptimizationMethod):
         residual = np.linalg.norm(self.grad(x))
         if residual < self.tol:
             criterion = True
-        # if self.counter > self.k:
-        #     print("hello")
-        #     criterion = True
+        if self.counter > self.k:
+            print("hello")
+            criterion = True
         return criterion
     
     def cauchy_crit(self, x, x_new):
@@ -131,13 +131,13 @@ class ClassicalNewtonMethod(GeneralOptimizationMethod):
         return alpha_opt.x # x is solution array of the optimization result from minimize_scalar
     
     def armijo(self, x, s_k, alpha, sigma):
-        if np.linalg.norm(self.func(x + alpha * s_k)) <= np.linalg.norm(self.func(x) + sigma * alpha * (s_k * self.grad(x))):
+        if self.func(x + alpha * s_k) <= self.func(x) + sigma * alpha * np.dot(s_k, self.grad(x)):
             return True
         else:
             return False
         
     def wolfe(self, x, s_k, alpha, rho):
-        if np.linalg.norm((s_k * self.grad(x + alpha * s_k))) >= np.linalg.norm(rho * (s_k * self.grad(x))):
+        if np.dot(s_k, self.grad(x + alpha * s_k)) >= rho * np.dot(s_k, self.grad(x)):
             return True
         else:
             return False
