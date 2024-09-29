0e187c48d3a72cc8225968d1170592ca16bd6645
grimmjule <grimmjule262@gmail.com>
2024-09-29 11:27:12 +0200

    testing(Task11)

--- Task11.py
+++ Task11.py
@@ -7,19 +7,20 @@ import Task1
 
 
 # define initial guess (x0)
-x0 = np.array([0, 1, 5])
-#x0 = np.linspace(0,1,3)
+x0 = np.array([2, 3, 5, 3])
+# x0 = np.linspace(0, 1, 8)
+
 
 # define initial hess
-hess = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+hess = np.eye(4)
 
-test_run = Task1.BFGS(ch.chebyquad, ch.gradchebyquad, x0, k=500)
-minimizer, steps = test_run.optimization_inexact_ls(sigma=10**(-2), rho=0.9, alpha_min=5, hess=hess)
+test_run = Task1.BFGS(ch.chebyquad, ch.gradchebyquad, x0, k=1000, tol=1e-5, steep=True)
+minimizer, steps = test_run.optimization_inexact_ls(sigma=0.1, rho=0.01, alpha_min=5, hess=hess)
 
+# scipy optimization
 xmin= so.fmin_bfgs(ch.chebyquad, x0, ch.gradchebyquad)
 
-print(steps, flush=True)
+# print(steps, flush=True)
 print(f'our x_min {minimizer}')
 print(f'scipys min {xmin}')
-
 
